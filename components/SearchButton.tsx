25b303b966cefd95c66a4bfbe932e4c6805eee01
Sara Vieira <hey@iamsaravieira.com>
2024-09-06 11:59:32 +0100

    feat: move search to trieve

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,21 +1,14 @@
-import { AlgoliaButton } from 'pliny/search/AlgoliaButton'
-import { KBarButton } from 'pliny/search/KBarButton'
 import siteMetadata from '@/data/siteMetadata'
-import { SearchIcon } from 'lucide-react'
+import { TrieveModalSearch, TrieveSDK } from 'trieve-search-component'
+import 'trieve-search-component/dist/index.css'
 
+const trieve = new TrieveSDK({
+  apiKey: 'tr-l1IRx4Jw0iVICiFdf9NroFwmDWQ4CnEd',
+  datasetId: '85bdeb65-44ec-4c9c-9d64-725601ad672a',
+})
 const SearchButton = () => {
-  if (
-    siteMetadata.search &&
-    (siteMetadata.search.provider === 'algolia' || siteMetadata.search.provider === 'kbar')
-  ) {
-    const SearchButtonWrapper =
-      siteMetadata.search.provider === 'algolia' ? AlgoliaButton : KBarButton
-
-    return (
-      <SearchButtonWrapper aria-label="Search" className="px-2 py-2 bg-signoz_slate-500 rounded-full">
-        <SearchIcon size={16} />
-      </SearchButtonWrapper>
-    )
+  if (siteMetadata.search) {
+    return <TrieveModalSearch theme="dark" trieve={trieve} />
   }
 }
 
