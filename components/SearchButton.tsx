28cea145f70fd121074d994102d09a4322af0975
Sara Vieira <hey@iamsaravieira.com>
2024-09-12 14:43:06 +0100

    chore: update search component to latest version

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -8,7 +8,30 @@ const trieve = new TrieveSDK({
 })
 const SearchButton = () => {
   if (siteMetadata.search) {
-    return <TrieveModalSearch theme="dark" trieve={trieve} />
+    return (
+      <TrieveModalSearch
+        theme="dark"
+        trieve={trieve}
+        ButtonEl={() => (
+          <button aria-label="Search" className="rounded-full bg-signoz_slate-500 px-2 py-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </svg>
+          </button>
+        )}
+      />
+    )
   }
 }
 
